f1da0f010bfdca159af05600e6ead5acf71a40e8
Archit Rungta <architrungta120@gmail.com>
2022-01-15 14:44:26 +0530

    add more tests and bugfix on vc++

--- src/infrastructure/UnreliableNetworkLayer/unreliable_network_layer.cpp
+++ src/infrastructure/UnreliableNetworkLayer/unreliable_network_layer.cpp
@@ -10,7 +10,9 @@ UnreliableNetworkLayer::UnreliableNetworkLayer(double prob_loss, double prob_cor
 
     loss_distribution = bernoulli_distribution(prob_loss);
     corrupt_distribution = bernoulli_distribution(prob_corrupt);
-    delay_distribution = geometric_distribution<int>(1.0 / expected_delay);
+
+    if (_expected_delay != 1)
+        delay_distribution = geometric_distribution<int>(1.0 / expected_delay);
 }
 
 //send data into unreliable channel
@@ -22,24 +24,26 @@ int UnreliableNetworkLayer::send(const void *msg, int len)
     shared_ptr<uint8_t> temp_buffer(new uint8_t[len]);
     memcpy((void *)temp_buffer.get(), msg, len);
     for (int i = 0; i < len; i++)
-        if(corrupt_distribution(generator))
+        if (corrupt_distribution(generator))
             temp_buffer.get()[i] = (uint8_t)rand();
-    
-    if(_expected_delay==1) {
+
+    if (_expected_delay == 1)
+    {
         lock_guard<mutex> lg(m);
         message_queue.push(make_pair(temp_buffer, len));
         return len;
     }
 
-    int delay = delay_distribution(generator)+1;
+    int delay = delay_distribution(generator) + 1;
 
     // no timers in Cpp so create a new thread and detach
-    
-    thread timerthread([delay, temp_buffer, len, this]() {
-        this_thread::sleep_for(chrono::milliseconds(delay));
-        lock_guard<mutex> lg(m);
-        message_queue.push(make_pair(temp_buffer, len));
-    });
+
+    thread timerthread([delay, temp_buffer, len, this]()
+                       {
+                           this_thread::sleep_for(chrono::milliseconds(delay));
+                           lock_guard<mutex> lg(m);
+                           message_queue.push(make_pair(temp_buffer, len));
+                       });
 
     timerthread.detach();
 
@@ -50,7 +54,8 @@ int UnreliableNetworkLayer::send(const void *msg, int len)
 int UnreliableNetworkLayer::recv(void *buf, int len)
 {
     lock_guard<mutex> lg(m);
-    if(message_queue.size()==0) return 0;
+    if (message_queue.size() == 0)
+        return 0;
     auto top = message_queue.front();
     message_queue.pop();
     int alen = min(len, top.second);
--- tests/unreliable_network_layer_test.cpp
+++ tests/unreliable_network_layer_test.cpp
@@ -2,13 +2,67 @@
 #include "UnreliableNetworkLayer/unreliable_network_layer.h"
 
 // Demonstrate some basic assertions.
-TEST(UnreliableNetworkLayer, TotallyReliable) {
-    uint8_t* data = new uint8_t[100]();
-    UnreliableNetworkLayer totallyReliable(0, 0, 100);
+TEST(UnreliableNetworkLayer, TotallyReliable)
+{
+    uint8_t *data = new uint8_t[100]();
+    for (int i = 0; i < 100; i++)
+        data[i]=i;
+
+    UnreliableNetworkLayer totallyReliable(0, 0);
     totallyReliable.send(data, 100);
     this_thread::sleep_for(chrono::milliseconds(100));
-    totallyReliable.recv(data, 100);
+    ASSERT_EQ(totallyReliable.recv(data, 100), 100);
 
-    for(int i =0;i<100;i++)
-        ASSERT_EQ(data[i], 0);
+    for (int i = 0; i < 100; i++)
+        ASSERT_EQ(data[i], i);
+}
+
+TEST(UnreliableNetworkLayer, LossyStream)
+{
+    uint8_t *data = new uint8_t[100]();
+    UnreliableNetworkLayer totallyReliable(0.5, 0);
+    for (int i = 0; i < 100; i++)
+        totallyReliable.send(data, 1);
+    this_thread::sleep_for(chrono::milliseconds(100));
+    int received = 0, x = 0;
+    while ((x = totallyReliable.recv(data, 100)))
+        received += x;
+
+    ASSERT_NE(received, 100);
+}
+
+TEST(UnreliableNetworkLayer, CorruptingStream)
+{
+    uint8_t *data = new uint8_t[100]();
+    UnreliableNetworkLayer totallyReliable(0, 0.5);
+    for (int i = 0; i < 100; i++)
+        totallyReliable.send(data, 1);
+    this_thread::sleep_for(chrono::milliseconds(100));
+    int received = 0, x = 0;
+    bool uneq = false;
+    for (int i = 0; i < 100; i++)
+    {
+        totallyReliable.recv(data, 1);
+        uneq = (uneq||data[0]!=i);
+    }
+
+    ASSERT_TRUE(uneq);
+}
+
+TEST(UnreliableNetworkLayer, ReorderingStream)
+{
+    uint8_t *data = new uint8_t[100]();
+    UnreliableNetworkLayer totallyReliable(0, 0, 5);
+    for (int i = 0; i < 100; i++)
+        totallyReliable.send(data, 1);
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    int received = 0, x = 0;
+    bool uneq = false;
+    for (int i = 0; i < 100; i++)
+    {
+        totallyReliable.recv(data, 1);
+        uneq = (uneq||data[0]!=i);
+    }
+
+    ASSERT_TRUE(uneq);
 }
